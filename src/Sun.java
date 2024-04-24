ce75e4d2428ed8beef1d9189b7e2674f9b5471a7
Shuulker <JosephSparrowRoberts26@gmail.com>
2024-04-24 13:20:16 -0700

    Completed Java Lab 012

---
+++ src/Sun.java
@@ -0,0 +1,44 @@
+class Sun {
+    private String name;
+    private double radius;
+    private double mass;
+    private double temp;
+    private double x;
+    private double y;
+
+    public Sun(String name, double radius, double mass, double temp) {
+        this.name = name;
+        this.radius = radius;
+        this.mass = mass;
+        this.temp = temp;
+        this.x = 0; // Initial x position assumed to be 0
+        this.y = 0; // Initial y position assumed to be 0
+    }
+
+    public double getXVel() {
+        return 0; // Sun is assumed to be stationary
+    }
+
+    public double getYVel() {
+        return 0; // Sun is assumed to be stationary
+    }
+
+    public double getXPos() { return 0; }
+    public double getYPos() { return 0; }
+
+    public double getMass() {
+        return mass;
+    }
+
+    @Override
+    public String toString() {
+        return "Sun{" +
+                "name='" + name + '\'' +
+                ", radius=" + radius +
+                ", mass=" + mass +
+                ", temp=" + temp +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
